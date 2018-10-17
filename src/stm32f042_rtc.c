02c6c4ae5013164f08db0a85735e8487e2d83e2c
Tomek <tomasz.pluta.g2@gmail.com>
2018-10-17 23:03:58 +0200

    Enabled RTC domain

--- src/stm32f042_rtc.c
+++ src/stm32f042_rtc.c
@@ -21,12 +21,35 @@ void RtcUnlock(void){
 
 
 void RtcInit(void){
-	RtcUnlock();
-	RTC->ISR  |= RTC_ISR_INIT;
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	RCC->CSR |= RCC_CSR_LSION;
+	PWR->CR |= PWR_CR_DBP;
+	RCC->BDCR |= RCC_BDCR_RTCEN;
+	RCC->BDCR |= RCC_BDCR_RTCSEL_LSI;
+	RTC->WPR = 0xCA; /* (1) */
+	RTC->WPR = 0x53; /* (1) */
+	RTC->ISR |= RTC_ISR_INIT; /* (2) */
 	while ((RTC->ISR & RTC_ISR_INITF) != RTC_ISR_INITF) /* (3) */
 	{
 	/* add time out here for a robust application */
 	}
-	RTC->ISR  &= ~RTC_ISR_INIT;
-//	RTC->PRER =
+	RTC->PRER = 0x007F0137; /* (4) */
+	RTC->TR = RTC_TR_PM | 10; /* (5) */
+	RTC->ISR &=~ RTC_ISR_INIT; /* (6) */
+	RTC->WPR = 0xFE; /* (7) */
+	RTC->WPR = 0x64; /* (7) */
+
+
+
+
+//	RtcUnlock();
+//	RTC->ISR  |= RTC_ISR_INIT;
+//	//uint32_t wait = RTC_INIT_TIMEOUT;
+//	//while ((RTC->ISR & RTC_ISR_INITF) != RTC_ISR_INITF)  && --wait))/* (3) */
+//	while ((RTC->ISR & RTC_ISR_INITF) != RTC_ISR_INITF) /* (3) */
+//	{
+//	/* add time out here for a robust application */
+//	}
+//	RTC->ISR  &= ~RTC_ISR_INIT;
+////	RTC->PRER =
 }
